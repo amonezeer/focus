5e04cc9654522f2ee8910d0a316238e49090edac
Yusip_ph14@student.itstep.org <Yusip_ph14@student.itstep.org>
2024-12-19 13:08:44 +0200

    Добавьте файлы проекта.

---
+++ dz 17 12/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Focus
+{
+    public partial class MainWindow : Window
+    {
+        public static readonly RoutedCommand ChangeColorCommand = new RoutedCommand();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CommandBinding colorCommandBinding = new CommandBinding(ChangeColorCommand, ChangeColorExecuted);
+            this.CommandBindings.Add(colorCommandBinding);
+
+            KeyGesture keyGesture = new KeyGesture(Key.R, ModifierKeys.Control);
+            InputBinding inputBinding = new InputBinding(ChangeColorCommand, keyGesture);
+            this.InputBindings.Add(inputBinding);
+        }
+
+        private void ChangeColorExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            SetFocusedControlColor();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetFocusedControlColor();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SetFocusedControlColor();
+        }
+
+        private void SetFocusedControlColor()
+        {
+            if (Keyboard.FocusedElement is Control focusedControl)
+            {
+                focusedControl.Background = Brushes.Red;
+            }
+        }
+    }
+}
